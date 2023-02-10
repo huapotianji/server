1171b1b517a3bf2146a3ab6b92c79b5225c3bb35
zhaohaitao <zachzhao@soarinfotech.com>
2023-02-10 17:01:21 +0800

    chat

--- controller/chat/chat_test.js
+++ controller/chat/chat_test.js
@@ -1,3 +1,8 @@
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
 
 
 
@@ -9,19 +14,24 @@
 
 
 
-
-
-
-
-function test(ctx) {
-    console.log('test')
-    ctx.body = {code:1,msg:'成功'}
+async function test(ctx) {
+    let input = ctx.request.query.input
+    if (!input) {
+        ctx.body = { code: 0, msg: '请输入有效值', response: '' }
+    }
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: input,
+        max_tokens: 2048,
+        temperature: 0,
+      });
+    ctx.body = { code: 1, msg: '成功', response: response.data }
 }
 
 
 
 
 
-module.exports ={
+module.exports = {
     test
 }
